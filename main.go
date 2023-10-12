499d9b899abf0a23811154679dab329ab9d76b36
Edward Suwirya <edward_suwirya@yahoo.com>
2023-10-12 11:59:56 +0700

    Change text message

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	r := gin.Default()
 	r.GET("/", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "My very new Enigmacamp",
+			"message": "My very new Hello World",
 		})
 	})
 	r.GET("/ping", func(c *gin.Context) {
